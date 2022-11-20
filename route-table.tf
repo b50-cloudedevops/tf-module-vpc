b52f56e13bab52274115a93abf74ff6645c34964
vamsi434 <vamsibusa@gmail.com>
2022-11-20 14:33:50 +0530

    ths

--- route-table.tf
+++ route-table.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0"
-    gateway_id = gateway_id.igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
    tags = {
