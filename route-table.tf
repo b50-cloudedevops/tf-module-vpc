eac74b857de9bb21283bb1d28cc5630cd3ea5e4f
vamsi434 <vamsibusa@gmail.com>
2022-11-20 18:54:26 +0530

    id

--- route-table.tf
+++ route-table.tf
@@ -44,5 +44,5 @@ resource "aws_route_table_association" "private-rt-association" {
 resource "aws_route" "r" {
   route_table_id            = var.DEFAULT_VPC_RT
   destination_cidr_block    = var.DEFAULT_VPC_CIDR
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
