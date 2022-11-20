d3ce24c97731e2b717d1288b77b6990cb2a114d6
vamsi434 <vamsibusa@gmail.com>
2022-11-20 19:17:50 +0530

    ta

--- route-table.tf
+++ route-table.tf
@@ -43,6 +43,6 @@ resource "aws_route_table_association" "private-rt-association" {
 #add route on the default vpcs public subnet route table
 resource "aws_route" "r" {
   route_table_id            = var.DEFAULT_VPC_RT
-  destination_cidr_block    = var.DEFAULT_VPC_CIDR
+  destination_cidr_block    = var.VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
