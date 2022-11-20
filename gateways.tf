9ea5855f9ebac6cc6ec630bc7102cc4269a1e396
vamsi434 <vamsibusa@gmail.com>
2022-11-20 20:20:35 +0530

    e

--- gateways.tf
+++ gateways.tf
@@ -1,6 +1,24 @@
+# creates internet gateway
 resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.main.id
     tags =  {
         Name = "${var.ENV}-igw"
     }
+}
+# allocates elastic iprovider 
+resource "aws_eip" "ngw_eip" {
+    vpc = true
+}
+
+
+#created NAT Gateway
+resource "aws_nat_gateway" "ngw" {
+    allocation_id =  aws_eip.ngw-eip.id 
+    subnet_id = aws_subnet.public.*.id[0] 
+    tags =  {
+        Name = "${var.ENV}-ngw"
+    }
+    depends_on = [
+       aws_internet_gateway.igw
+    ]
 }
--- route-table.tf
+++ route-table.tf
@@ -29,6 +29,10 @@ resource "aws_route_table" "private-rt" {
     cidr_block =  var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw.id 
+  }
 
    tags = {
     Name = "${var.ENV}-prv-route-table"
