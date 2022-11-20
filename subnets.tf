b94f89b4a3e33bb4134b4e0a3da54ceac0f6b2c8
vamsi434 <vamsibusa@gmail.com>
2022-11-20 11:32:14 +0530

    ths

--- subnets.tf
+++ subnets.tf
@@ -2,7 +2,8 @@ resource "aws_subnet" "pub-main1" {
     vpc_id = aws_vpc.main.id
     count = length(var.PUBLIC_SUBNET_CIDR)
     cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+    availability_zone = element(var.AZ, count.index)
     tags = {
-        Name = "pub-main1"
+        Name = "${var.ENV}-${element(var.AZ, count.index)}"
     }
 }
